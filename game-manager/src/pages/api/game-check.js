ffd83c734deb1694da81b3d345029b9ff67f7818
Jérôme Marichez <jeromemarichez@ik.me>
2025-01-24 12:29:04 +0100

    fix: :sparkles: Fix nextjs prod

--- game-manager/src/pages/api/game-check.js
+++ game-manager/src/pages/api/game-check.js
@@ -4,8 +4,8 @@ import path from 'path';
 
 export default async function handler(req, res) {
   
-  const gameDataPath = path.join(process.cwd(), 'data', 'gameData.json');
-  const gameWinPath = path.join(process.cwd(), 'data', 'gameWin.json');
+  const gameDataPath = path.join(process.cwd(), '/app/', 'gameData.json');
+  const gameWinPath = path.join(process.cwd(), '/app/', 'gameWin.json');
 
   try {
     const gameData = JSON.parse(await fs.readFile(gameDataPath, 'utf-8'));
--- game-manager/src/pages/api/save-data.js
+++ game-manager/src/pages/api/save-data.js
@@ -3,16 +3,16 @@ import path from 'path';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const filePath = path.join(process.cwd(), 'data', 'gameData.json');
+    const filePath = path.join(process.cwd(), '/app/data', 'gameData.json');
 
     try {
       await fs.writeFile(filePath, JSON.stringify(req.body, null, 2));
-      res.status(200).json({ message: 'Donnée sauvegarder !' });
+      res.status(200).json({ message: 'Donnée sauvegardée !' });
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: 'Erreur en sauvegardant les données' });
     }
   } else {
-    res.status(405).json({ message: 'Méthode non autoriser' });
+    res.status(405).json({ message: 'Méthode non autorisée' });
   }
 }
