57ba7871eeea30e610e6b4379b9105af9d4dc7b3
Jérôme Marichez <jeromemarichez@ik.me>
2025-05-02 10:49:10 +0200

    feat: :sparkles: Add a copy of the website

---
+++ src/pages/api/marketing5/save-data.js
@@ -0,0 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const filePath = "./gameData5.json";
+
+    try {
+      await fs.writeFile(filePath, JSON.stringify(req.body, null, 2));
+      res.status(200).json({ message: 'Donnée sauvegardée !' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Erreur en sauvegardant les données' });
+    }
+  } else {
+    res.status(405).json({ message: 'Méthode non autorisée' });
+  }
+}
